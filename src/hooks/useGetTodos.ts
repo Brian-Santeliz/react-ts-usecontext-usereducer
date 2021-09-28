39e173b5df52e26b47de702ff028c64ee02dd2bb
Brian <blsvsanteliz@gmail.com>
2021-09-28 10:47:19 -0400

    Create custom hook useGetTodos

---
+++ src/hooks/useGetTodos.ts
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { todoContext } from "../context/Context";
+export const useGetTodos = () => {
+  const {
+    todoState: { todos },
+    toggleTodo,
+    addTodo,
+  } = useContext(todoContext);
+  return {
+    todos,
+    pending: todos.filter((todo) => !todo.done),
+    toggleTodo,
+    addTodo,
+  };
+};
