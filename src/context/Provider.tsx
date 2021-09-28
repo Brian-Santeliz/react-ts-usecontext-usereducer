8237dd2addca6bfc2177163608026bd33e817558
Brian <blsvsanteliz@gmail.com>
2021-09-27 23:47:30 -0400

    Create Provider.tsx, add useReducer and action toggle & add todo, add Initial_state

---
+++ src/context/Provider.tsx
@@ -0,0 +1,47 @@
+import { todoContext } from "./Context";
+import { useReducer } from "react";
+import { Todo, TodoState } from "../interfaces";
+import { todoReducer, todoType } from "./Reducer";
+interface ContextProviderProp {
+  children: JSX.Element | JSX.Element[];
+}
+
+const INITIAL_STATE: TodoState = {
+  todos: [
+    {
+      id: 1,
+      description: "How use TS with React",
+      done: false,
+      name: "Learn Typescript",
+    },
+  ],
+  pending: 1,
+};
+export const ContextProvider = ({ children }: ContextProviderProp) => {
+  const [stateTodo, dispatch] = useReducer(todoReducer, INITIAL_STATE);
+  const toggleTodo = (id: number) => {
+    dispatch({
+      type: todoType.TOGGLE_TODO,
+      payload: { id },
+    });
+  };
+  const addTodo = (todo: Todo) => {
+    dispatch({
+      type: todoType.ADD_TODO,
+      payload: {
+        ...todo,
+      },
+    });
+  };
+  return (
+    <todoContext.Provider
+      value={{
+        todo: [],
+        addTodo,
+        toggleTodo,
+      }}
+    >
+      {children}
+    </todoContext.Provider>
+  );
+};
